61717afe8208ce0ac25f5dffc2d7cecb12a5a1af
Alejandro <alpermar98@gmail.com>
2020-12-22 19:37:05 +0100

    Created Scripts and Prefabs folders, created GameManager singleton

---
+++ VDM-P2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Public 
+
+    // Private 
+    private static GameManager _instance; // Singleton instance
+
+    // Init singleton in Awake
+    private void Awake() {
+        if(_instance == null){
+            // Save GameManager instance
+            _instance = this; 
+
+            // TODO: Implement initialization
+            // ...
+        } // if
+        else if (_instance != this) {
+            Destroy(gameObject);
+        } // else if
+    } // Awake
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    } // Start
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } // Update
+} // GameManager
