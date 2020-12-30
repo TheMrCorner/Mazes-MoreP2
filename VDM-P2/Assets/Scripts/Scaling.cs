a22dbd01cdb7773b75bbc7019a7c5a53d1449207
Alejandro <alpermar98@gmail.com>
2020-12-30 22:06:42 +0100

    Testing...

---
+++ VDM-P2/Assets/Scripts/Scaling.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity;
+using UnityEngine;
+
+public class Scaling
+{
+    // Reference resolution and current resolution
+    Vector2 _refResolution;
+    Vector2 _currResolution;
+
+    // Factor to change between Unity Units and pixels
+    float _unityUds;
+} // Scaling
