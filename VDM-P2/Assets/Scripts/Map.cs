bbaa345918d692683f112ca13e96476f1af9bf1d
bsolla <bsolla@ucm.es>
2021-01-02 04:13:43 +0100

    Added the tile prefab and some classes

---
+++ VDM-P2/Assets/Scripts/Map.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map
+{
+    int width, height;
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    // TODO: fill with the reading
+    public static Map FromJson(string json)
+    {
+        return null;
+    }
+}
