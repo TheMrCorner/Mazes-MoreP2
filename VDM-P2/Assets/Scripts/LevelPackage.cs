8efe77d6a19ff88eb34ca9f98998326f43dfff22
bsolla <bsolla@ucm.es>
2021-01-14 18:06:03 +0100

    Added level packages

---
+++ VDM-P2/Assets/Scripts/LevelPackage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "LevelPackage")]
+public class LevelPackage : ScriptableObject
+{
+    public TextAsset[] levels;
+    public Color playerColor;
+}
