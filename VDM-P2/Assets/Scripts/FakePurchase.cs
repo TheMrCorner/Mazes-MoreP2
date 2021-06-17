fb9a943825d79cf349032419b47c007ccac67674
bSolla <bsolla@ucm.es>
2021-06-17 02:46:29 +0200

    Added a script for fake buying in Android

---
+++ VDM-P2/Assets/Scripts/FakePurchase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakePurchase : MonoBehaviour
+{
+    public void PurchaseNoAdds()
+    {
+#if UNITY_ANDROID
+        GameManager.GetInstance().RemoveAds();
+#endif
+#if UNITY_EDITOR
+        Debug.LogWarning("Fake Purchase is only for Android");
+#endif
+    }
+
+    public void PurchaseHints(int nHints)
+    {
+#if UNITY_ANDROID
+        GameManager.GetInstance().AddHints(nHints);
+#endif
+#if UNITY_EDITOR
+        Debug.LogWarning("Fake Purchase is only for Android");
+#endif
+    }
+}
